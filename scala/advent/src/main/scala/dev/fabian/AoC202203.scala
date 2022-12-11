86503981097235ba042cc60083ac6b1dbdb0b715
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2022-12-10 18:48:49 -0600

    [aoc-2022][scala] Refactor Day 3 Solution

--- scala/advent/src/main/scala/dev/fabian/AoC202203.scala
+++ scala/advent/src/main/scala/dev/fabian/AoC202203.scala
@@ -1,35 +1,33 @@
 package dev.fabian
 
-import scala.io.StdIn.*
+import cats.effect.{IO, IOApp}
 
-object AoC202203 extends App {
-  def common(input: String) = {
-    val (sackA, sackB) = input.splitAt(input.length() / 2)
-    val common = (sackA.toSet intersect sackB.toSet)
-    common.head
-  }
-
+object AoC202203 extends IOApp.Simple {
   def priority(c: Char): Int =
     c.toInt - (if (c.isLower) 96 else 38)
 
-  def loopA(acc: Long): Long = {
-    val input = readLine()
-    if (input.isEmpty()) acc else loopA(acc + priority(common(input)))
-  }
+  override def run: IO[Unit] = for {
+    entries <- read("202203").filter(_.nonEmpty).compile.toList
 
-  def loopB(acc: Long): Long = {
-    val inputA = readLine()
-    if (inputA.isEmpty()) acc
-    else {
-      val inputB = readLine()
-      val inputC = readLine()
-      val common =
-        (inputA.toSet intersect inputB.toSet intersect inputC.toSet).head
+    part1 = entries
+              .map { e =>
+                val (sackA, sackB) = e.splitAt(e.length() / 2)
+                val c              = (sackA.toSet intersect sackB.toSet).head
+                priority(c)
+              }
 
-      loopB(acc + priority(common))
-    }
-  }
+    part2 = entries
+              .grouped(3)
+              .map {
+                case Seq(e1, e2, e3) =>
+                  val set = e1.toSet intersect e2.toSet intersect e3.toSet
+                  val c   = set.head
+                  priority(c)
 
-  println(loopA(0))
-  println(loopB(0))
+                case _ => ???
+              }
+
+    _ <- IO.println(s"Part 1: ${part1.sum}")
+    _ <- IO.println(s"Part 1: ${part2.sum}")
+  } yield ()
 }
