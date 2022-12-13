dbb16bf34e731e756679288d6ae883a9960cef94
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2022-12-12 23:03:15 -0600

    [aoc-2022][scala] Initial Day 6 solution

---
+++ scala/advent/src/main/scala/dev/fabian/AoC202206.scala
@@ -0,0 +1,33 @@
+package dev.fabian
+
+import scala.annotation.tailrec
+
+import cats.effect.{IO, IOApp}
+import cats.parse.{Parser, Parser0}
+
+object AoC202206 extends IOApp.Simple {
+  def anyNotIn(chars: Set[Char]): Parser[Char] =
+    Parser.charWhere(c => !(chars contains c))
+
+  def nDifferent(n: Int, notIn: Set[Char] = Set.empty): Parser[Int] =
+    if (n > 1)
+      for {
+        newChar <- anyNotIn(notIn)
+        r       <- nDifferent(n - 1, notIn + newChar)
+      } yield r
+    else anyNotIn(notIn) *> Parser.index
+
+  def find(n: Int): Parser[Int] = Parser.recursive { recurse =>
+    nDifferent(n).backtrack | (Parser.anyChar *> recurse)
+  }
+
+  override def run: IO[Unit] = for {
+    input <- read("202206").compile.toList.map(_.head)
+
+    part1 = find(4).parse(input).toOption.get._2
+    part2 = find(14).parse(input).toOption.get._2
+
+    _ <- IO.println(s"Part 1: $part1")
+    _ <- IO.println(s"Part 2: $part2")
+  } yield ()
+}
